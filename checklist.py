7063749b3f09e9b92d5842a09bf6b00ab8af3059
NoblePrinceIII <farhan.begg@students.makeschool.com>
2019-08-29 15:25:33 -0700

    Added Destroy and Return Index

--- checklist.py
+++ checklist.py
@@ -1,6 +1,6 @@
 checklist = list()
 
-# CREATE
+# Define Functions
 def create(item):
     checklist.append(item)
 
@@ -9,12 +9,13 @@ def read(index):
     return checklist[index]
 
 # UPDATE
-def update(index, item):
+def update(index, item): #check index
     checklist[index] = item
 
 # DESTROY
 def destroy(index):
-    checklist.pop(index)
+    checklist.pop(int(index))
+    print(checklist)
 
 def list_all_items():
     index = 0
@@ -25,28 +26,40 @@ def mark_completed(index):
     checklist[index] = " √ " + checklist[index]
     # Add code here that marks an item as completed
 
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
 def select(function_code):
     # Create item
     if function_code == "C":
         input_item = user_input("Input item:")
         create(input_item)
     # Read item
-    elif function_code == "R":
-        item_index = user_input("Index Number?")
-        # Remember that item_index must actually exist or our program will crash.
-        read(item_index)
+    elif function_code == "I":
+        item_index = int(user_input("Index Number?"))
+    # Remember that item_index must actually exist or our program will crash.
+        print(read(item_index))
     # Print all items
     elif function_code == "P":
         list_all_items()
+    elif function_code == "D":
+        input_item = user_input("Destroy Index: ")
+        destroy(input_item)
+    elif function_code == "R":
+        item_index = int(user_input("Return Index: "))
+        read(item_index)
+        print(read(item_index))
+    #elif function_code
+        
+
+
+
     # Catch all
     else:
-        print("Unknown Option")
-
-def user_input(prompt):
-    # the input function will display a message in the terminal
-    # and wait for user input.
-    user_input = input(prompt)
-    return user_input
+        return True
 
 def test():
     create("purple sox")
@@ -63,4 +76,12 @@ def test():
 
     list_all_items()
 
+
+
 test()
+
+running = True
+while running:
+    selection = user_input(
+        "Press C to add to list, R to Read from list and P to display list Press D to Del Press R to Retrun Index: ")
+    select(selection.upper())
