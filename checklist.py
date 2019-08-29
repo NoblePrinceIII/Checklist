cdd9039cd8c0686127d368d04a0b127708d54ed5
NoblePrinceIII <farhanbegg@Farhans-MacBook-Air.local>
2019-08-29 13:06:14 -0700

    Check Mark

---
+++ checklist.py
@@ -0,0 +1,43 @@
+checklist = list()
+
+# CREATE
+def create(item):
+    checklist.append(item)
+
+# READ
+def read(index):
+    return checklist[index]
+
+# UPDATE
+def update(index, item):
+    checklist[index] = item
+
+# DESTROY
+def destroy(index):
+    checklist.pop(index)
+
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print("{} {}".format(index, list_item))
+
+def mark_completed(index):
+    checklist[index] = "√" + checklist[index]
+    # Add code here that marks an item as completed
+
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+
+    destroy(1)
+
+    print(read(0))
+
+    list_all_items()
+
+test()
