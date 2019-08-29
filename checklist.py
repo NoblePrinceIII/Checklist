9ddf2e5fbacb3ae38f6887e3b6364a7a7aa0a6df
NoblePrinceIII <farhanbegg@Farhans-MacBook-Air.local>
2019-08-29 13:43:30 -0700

    Added Functions select and user input

--- checklist.py
+++ checklist.py
@@ -22,8 +22,31 @@ def list_all_items():
         print("{} {}".format(index, list_item))
 
 def mark_completed(index):
-    checklist[index] = "√" + checklist[index]
+    checklist[index] = " √ " + checklist[index]
     # Add code here that marks an item as completed
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+        # Remember that item_index must actually exist or our program will crash.
+        read(item_index)
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+    # Catch all
+    else:
+        print("Unknown Option")
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
 
 def test():
     create("purple sox")
