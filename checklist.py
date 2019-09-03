6f4016799556263361b03ee653a27a866ae98535
nobleprinceiii <farhan.begg@students.makeschool.com>
2019-09-03 14:17:17 -0700

    Revision of Code, Simplified and Fixed Bugs, Final Product

--- checklist.py
+++ checklist.py
@@ -8,18 +8,12 @@ def create(item):
 
 # Read
 def read(index):
-    if len(Index) < 1:
-        print("No Index")
     return checklist[index]
 
 
 # Update
 def update(index, item):
     checklist[index] = item
-    if index > len(checklist):
-        print("Invalid")
-    else:
-        checklist[index] = item
 
 
 # Destroy
@@ -33,6 +27,7 @@ def list_all_items():
     index = 0
     for list_item in checklist:
         print("{} {}".format(index, list_item))
+        index += 1
 
 
 # marks item completed
@@ -54,36 +49,11 @@ def select(function_code):
         if function_code == "C":
             input_item = user_input("Input item: ")
             create(input_item)
-
-        # Shows Items in the Idex
-        elif function_code == "I":
-            item_index = int(user_input("Index Number? "))
-            if item_index + 1 < len(checklist):
-                print(read(item_index))
-            else:
-                print("invalid input")
-
-        # Shows all item on list
-        elif function_code == "P":
-            list_all_items()
-
-        # Destroy's an item in Index
-        elif function_code == "D":
-            item_index = int(user_input("Index Number: "))
-            if item_index + 1 > len(checklist):
-                print("Doesn't Exist")
-            else:
-                destroy(item_index)
-
-        # Shows whats in Index
+            # Reads List
         elif function_code == "R":
             item_index = int(user_input("R to read index: "))
-            if item_index + 1 < len(checklist):
-                read(item_index)
-            else:
-                print("Doesn't Exist")
-
-        # Update Index
+            print(read(item_index))
+            # Update Index
         elif function_code == "U":
             try:
                 item_index = int(user_input("Update Index: "))
@@ -92,31 +62,38 @@ def select(function_code):
                 return True
             except ValueError:
                 print("Invalid Input")
+            # Destroy's an item in Index
+        elif function_code == "D":
+            item_index = int(user_input("Index Number: "))
+            destroy(item_index)
+        # Shows whats in Index
+        elif function_code == "P":
+            list_all_items()
 
-                # Allows user to complete list
+        # Allows user to checklist
         elif function_code == "X":
             item_index = int(user_input("X index to complete: "))
             mark_completed(item_index)
 
             return True
     except:
-        print("Invalid Notation")
+        print("Invalid Notation or No Index")
+
 # def test():
 #     create("purple sox")
 #     create("red cloak")
 #     print(read(0))
-#     print(read(1))
-#     update(0, "purple socks")
-#     destroy(1)
+#     print(read(1))#     destroy(1)
 #     print(read(0))
 #     list_all_items()
-
 # test()
+
+
 running = True
 
 while running:
     selection = user_input(
-        "Press C create list, I to view Index list P to display list Press " +
-        "D to Delete   Press R to Retrun Index Press U to Update Index " +
-        "Press X when completed: ")
+        "Press C create list, R to read Index " +
+        "Press U to Update Index P to " +
+        "display list Press D to Delete Press X when completed: ")
     select(selection.upper())
